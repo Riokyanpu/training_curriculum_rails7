7a42823e4dedb4bf224063f26c3e09e2d31f10db
Riokyanpu <kgst1216@gmail.com>
2024-03-18 11:16:31 +0900

    Issue 1~3修正　2回目
    controller.rb
    get_week 1箇所（５行目）
    wdayからwdaysに変更（２３行目）
    
    index.html.erb
    <div>(10 行目削除)

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_Week
     @plan = Plan.new
   end
 
@@ -20,7 +20,7 @@ class CalendarsController < ApplicationController
 
   def get_Week
     # 役割の内容を記述することが重要（week_days_labels:曜日のラベルが格納されていることを示す）
-    wday = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
   
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
